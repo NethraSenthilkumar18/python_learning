15f915d7233bc70ffb4ed0a23e1cd4eff1462be3
NethraSenthilkumar18 <nethra3318@gmail.com>
2026-09-24 20:48:38 +0530

    Added my first python program

---
+++ day2.py
@@ -0,0 +1,6 @@
+name = "Nethra"
+course = "B.Tech AIDS"
+year = "1 st year"
+print("Hello, I, am, studing", course)
+print("I, am, currently, in",year")
+print("This is my day 2 Github contribution")
