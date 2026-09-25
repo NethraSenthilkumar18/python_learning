44ba05d0fd654dd0d6d0d9324d39481b78fe9456
NethraSenthilkumar18 <nethra3318@gmail.com>
2026-09-25 22:46:59 +0530

    Add Rock Paper Scissors game implementation
    My Day 3 Python mini project — Rock Paper Scissors!
    This project uses user input and random choices to create a simple game between the player and computer.

--- rock-paper-scissors.py
+++ rock-paper-scissors.py
@@ -8,9 +8,33 @@ print("3. Scissors")
 
 user_choice = input("Enter your choice: ")
 
+if user_choice == "1":
+    user_choice = "Rock"
+elif user_choice == "2":
+    user_choice = "Paper"
+elif user_choice == "3":
+    user_choice = "Scissors"
+else:
+    print("Invalid choice!")
+
 choices = ["Rock", "Paper", "Scissors"]
 
 computer_choice = random.choice(choices)
 
 print("You chose:", user_choice)
 print("Computer chose:", computer_choice)
+
+if user_choice == computer_choice:
+    print("It's a draw!")
+
+elif user_choice == "Rock" and computer_choice == "Scissors":
+    print("You win!")
+
+elif user_choice == "Paper" and computer_choice == "Rock":
+    print("You win!")
+
+elif user_choice == "Scissors" and computer_choice == "Paper":
+    print("You win!")
+
+else:
+    print("Computer wins!")
